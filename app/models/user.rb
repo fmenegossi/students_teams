290f432a3259f383dd3c41229c1d23f3d0f43c05
Felipe Menegossi <felipe.menegossi@gmail.com>
2018-01-29 16:17:38 +0100

    Define some User methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,18 @@ class User < ApplicationRecord
   has_many :user_matches , dependent: :destroy
   has_many :matches, through: :user_matches
 
+  def self.students
+    User.all.where(is_admin: false)
+  end
+
+  def self.admins
+    User.all.where(is_admin: true)
+  end
+
+  def past_matches
+    matches.where("date <= ?", Date.today)
+  end
+
   def is_admin?
     is_admin
   end
