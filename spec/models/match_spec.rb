80a3989f52115545801af843449c8640b28e313e
Felipe Menegossi <felipe.menegossi@gmail.com>
2018-01-29 13:08:12 +0100

    Define some tests

--- spec/models/match_spec.rb
+++ spec/models/match_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Match, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "CRUD Match" do
+    let (:match){ create(:match) }
+
+    it "add a new match" do
+      match.valid?
+      expect(Match.last).to eq match
+    end
+
+    it "can't add a match without a date"
+
+    it "can't add a match in the past"
+
+    it "destroy a match"
+  end
+
+  describe "Relations" do
+    let(:match){ create(:match) }
+    let(:user1){ create(:user) }
+    let(:user2){ create(:user) }
+    let(:user_matches){ create(:user_matches) }
+
+    it "users must be related to the match"
+
+    it "match must be related to the users"
+
+    it "destroy a match must destroy user_matches"
+
+    it "destroy a user must destroy user_matches"
+  end
 end
