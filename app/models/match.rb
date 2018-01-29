2821c5a073d45ac4e43a88cb01b2f6ce30cbbe06
Felipe Menegossi <felipe.menegossi@gmail.com>
2018-01-29 14:01:27 +0100

    Add match tests

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,4 +1,13 @@
 class Match < ApplicationRecord
   has_many :user_matches
   has_many :users, through: :user_matches
+
+  validates :date, presence: true
+  validate :date_cannot_be_in_the_past
+
+  def date_cannot_be_in_the_past
+    if date.present? && date < Date.today
+      errors.add(:date, "can't be in the past")
+    end
+  end
 end
--- spec/models/match_spec.rb
+++ spec/models/match_spec.rb
@@ -9,21 +9,48 @@ RSpec.describe Match, type: :model do
       expect(Match.last).to eq match
     end
 
-    it "can't add a match without a date"
+    it "can't add a match without a date" do
+      match.date = nil
+      match.valid?
+      expect(match.errors).to include(:date)
+    end
 
-    it "can't add a match in the past"
+    it "can't add a match in the past" do
+      match.date = Date.today - 1.days
+      match.valid?
+      expect(match.errors).to include(:date)
+    end
 
-    it "destroy a match"
+    it "destroy a match" do
+      match.valid?
+      expect(Match.last).to eq match
+      match.destroy!
+      expect(Match.last).not_to eq match
+    end
   end
 
   describe "Relations" do
     let(:match){ create(:match) }
     let(:user1){ create(:user) }
     let(:user2){ create(:user) }
-    let(:user_matches){ create(:user_matches) }
+    let(:user_match1){ create(:user_matches, user: user1, match: match) }
+    let(:user_match2){ create(:user_matches, user: user2, match: match) }
 
-    it "match must be related to the users"
+    it "match must be related to the users" do
+      user_match1.valid?
+      user_match2.valid?
 
-    it "destroy a match must destroy user_matches"
+      expect(user1.user_matches).to include(user_match1)
+      expect(user1.matches).to include(match)
+    end
+
+    it "destroy a match must destroy user_matches" do
+      user_match1.valid?
+      user_match2.valid?
+
+      expect(UserMatch.all.count).to eq(2)
+      match.destroy
+      expect(UserMatch.all.count).to eq(0)
+    end
   end
 end
