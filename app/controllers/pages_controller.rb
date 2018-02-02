1411c242ea0db08c0adc24cf2f46517c236c99fd
renato niola <niolarenato01@gmail.com>
2018-02-02 12:01:39 +0100

    style devise pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,13 @@ class PagesController < ApplicationController
 
   def home
     if current_user
+      
       redirect_to users_path
+
     end
   end
+
+
+
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,6 @@ class UsersController < ApplicationController
   before_action :set_user , only: [:index]
 
   def index
-
-    @role = @user.is_admin ? 'Admin' : 'Student'
-    
   end
 
   private
