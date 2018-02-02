82493c738004d9452bac2d8b47364ec43ee234ee
Felipe Menegossi <felipe.menegossi@gmail.com>
2018-02-02 11:19:43 +0100

    Fix algo. Create Group model

--- app/models/user_match.rb
+++ app/models/user_match.rb
@@ -25,7 +25,7 @@ class UserMatch < ApplicationRecord
 
   def self.build_mixed_teams(members, team_size)
     teams = []
-    members.shuffle!
+    members = members.shuffle
 
     members.each_slice(team_size){ |team| teams << team }
 
@@ -40,11 +40,11 @@ class UserMatch < ApplicationRecord
         other_students = User.all_other_students(student)
 
         past_matches = get_past_matches(student)
-        past_matches.where(user_id: other_students).pluck(:user_id)
+        past_matches = past_matches.where(user_id: other_students).pluck(:user_id)
         max_combinations = get_max_combinations(members.size, team_size)
 
         if (past_matches.include?(team_ids)) &&
-          ((past_matches.size % max_combinations > 0) || (last_match == team_ids))
+          (((past_matches.size % max_combinations) > 0))
           return build_mixed_teams(members, team_size)
         end
       end
@@ -77,6 +77,10 @@ class UserMatch < ApplicationRecord
 
   def self.get_max_combinations(number_of_members, team_size)
     other_members = number_of_members - 1
-    (other_members.downto(other_members - team_size)).inject(:*)
+    if team_size > 2
+      1 * (other_members.downto(other_members - (team_size - 1))).inject(:*)
+    else
+      other_members
+    end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,28 +9,3 @@ user5 = User.create!(email: 'michael@match.com', password: 'abcd1234')
 user6 = User.create!(email: 'tom@match.com', password: 'abcd1234')
 user7 = User.create!(email: 'sara@match.com', password: 'abcd1234')
 user8 = User.create!(email: 'sergio@match.com', password: 'abcd1234')
-
-match1 = Match.create!(date: Date.today)
-match2 = Match.create!(date: Date.today)
-match3 = Match.create!(date: Date.today)
-match4 = Match.create!(date: Date.today + 3.days)
-match5 = Match.create!(date: Date.today + 3.days)
-match6 = Match.create!(date: Date.today + 1.days)
-
-matchu1 = UserMatch.create!(match:match1, user: user2)
-matchu2 = UserMatch.create!(match:match1, user: user3)
-
-matchu3 = UserMatch.create!(match:match2, user: user4)
-matchu4 = UserMatch.create!(match:match2, user: user5)
-
-matchu5 = UserMatch.create!(match:match3, user: user6)
-matchu6 = UserMatch.create!(match:match3, user: user7)
-
-matchu7 = UserMatch.create!(match:match4, user: user8)
-matchu8 = UserMatch.create!(match:match4, user: user2)
-
-matchu9 = UserMatch.create!(match:match5, user: user2)
-matchu10 = UserMatch.create!(match:match5, user: user5)
-
-matchu11 = UserMatch.create!(match:match6, user: user2)
-matchu12 = UserMatch.create!(match:match6, user: user4)
